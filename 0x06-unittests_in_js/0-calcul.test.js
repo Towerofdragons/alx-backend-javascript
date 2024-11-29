df234f32f57bd3d7f96b291d2f626293ef270339
Mark <marknjoropge@gmail.com>
2024-11-29 19:51:06 +0000

    Add tests checking rounding for a and b

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,26 +7,11 @@ describe("addition test", function(){
         assert.equal(calculateNumber(2,3),5);
     });
 
-    it("check equality - float", function(){
-        assert.equal(calculateNumber(2.5, 2.5), 6);
+    it("rounds a", function(){
+        assert.equal(calculateNumber(2.5, 0), 3);
     });
 
-
-    it('should handle negative numbers', () => {
-        assert.strictEqual(calculateNumber(-1.4, -3.7), -5); // -1 + (-4) = -5
-        assert.strictEqual(calculateNumber(-1.5, -3.7), -6); // -2 + (-4) = -6
-        assert.strictEqual(calculateNumber(-1.6, -3.2), -5); // -2 + (-3) = -5
-      });
-    
-    it('should handle a mix of positive and negative numbers', () => {
-    assert.strictEqual(calculateNumber(-1.3, 3.7), 3); // -1 + 4 = 3
-    assert.strictEqual(calculateNumber(1.6, -3.2), -1); // 2 + (-3) = -1
-    assert.strictEqual(calculateNumber(-1.5, 2.5), 1); // -2 + 3 = 1
-    });
-
-    it('should handle zero', () => {
-    assert.strictEqual(calculateNumber(0, 0), 0); // 0 + 0 = 0
-    assert.strictEqual(calculateNumber(0, 3.7), 4); // 0 + 4 = 4
-    assert.strictEqual(calculateNumber(-1.5, 0), -2); // -2 + 0 = -2
+    it('rounds b', () => {
+        assert.strictEqual(calculateNumber(0, -3.7), -4); 
     });
 })
