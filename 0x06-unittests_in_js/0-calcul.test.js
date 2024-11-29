d0b42d7fc6843038ea37da97cc0a271eca9dd924
Mark <marknjoropge@gmail.com>
2024-11-29 19:33:04 +0000

    Task 0: Add new equals test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,7 +3,11 @@ const calculateNumber = require("./0-calcul");
 const assert = require("assert");
 
 describe("addition test", function(){
-    it("check equality", function(){
+    it("check equality - integers", function(){
         assert.equal(calculateNumber(2,3),5);
-    })
+    });
+
+    it("check equality - float", function(){
+        assert.equal(calculateNumber(2.5, 2.5), 6);
+    });
 })
