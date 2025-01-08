e2bbe807d5b45c29d27ef6160cce30e1e6852a20
Mark <marknjoropge@gmail.com>
2025-01-08 22:10:53 +0000

    Use \r for newline, add test for 1-stdin.test

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit();
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\r`);
+  process.exit();
 });
 
 process.on('exit', () => {
-    console.log("This important software is now closing");
+  console.log('This important software is now closing\r');
 });
