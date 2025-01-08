e08e483007c7e76cc44f18a721a7e8e36d1f1653
Mark <marknjoropge@gmail.com>
2025-01-08 22:59:14 +0000

    Fixed formatting for lint

--- 0x05-Node_JS_basic/1-stdin.test.js
+++ 0x05-Node_JS_basic/1-stdin.test.js
@@ -19,3 +19,16 @@ describe('main', () => {
     });
   });
 });
+
+describe('main', () => {
+    it('the user is entering a name', function (done) {
+      proc.stdout.once('data', (test) => {
+        expect(test.toString()).to.equal('Welcome to Holberton School, what is your name?\n');
+        proc.stdin.write('Guillaumeh\r');
+        proc.stdout.once('data', (test) => {
+          expect(test.toString()).to.equal('Your name is: Guillaumeh\r');
+          done();
+        });
+      });
+    });
+  });
