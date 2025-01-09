6beb8df749c85e65231cdec39d1222f73c6c8704
Mark <marknjoropge@gmail.com>
2025-01-09 06:18:59 +0000

    Fixed formatting for lint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
-const app = http.createServer(function(request, response){
-    response.write('Hello Holberton School!');
-    response.end();
+const app = http.createServer((request, response) => {
+  response.write('Hello Holberton School!');
+  response.end();
 }).listen(1245);
 
 module.exports = app;
