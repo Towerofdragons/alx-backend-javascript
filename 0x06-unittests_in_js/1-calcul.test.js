dee57fea1d7ce1b1dd66264cf1eebbd2df899533
Mark <marknjoropge@gmail.com>
2025-01-09 16:01:58 +0000

    Separate operations into describe blocks

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -8,11 +8,31 @@ describe("Test 1-calcul SUM", ()=> {
         assert.equal(calculateNumber("SUM",2,3),5);
     });
 
+    it("has correct output for DIVIDE", () => {
+        assert.equal(calculateNumber("DIVIDE",10,2),5);
+    });
+
+    it("has correct output for DIVIDE when b is 0", () => {
+        assert.equal(calculateNumber("DIVIDE",2,0), "Error");
+    })
+
+    it("has correct output for SUBTRACT", () => {
+        assert.equal(calculateNumber("SUBTRACT",5,2),3);
+    });
+
+    it("has correct output for SUBTRACT", () => {
+        assert.equal(calculateNumber("SUBTRACT",1.4,2.2),-1);
+    });
+
 });
 
 describe("Test 1-calcul SUBTRACT", ()=> {
     it("has correct output for SUBTRACT", () => {
         assert.equal(calculateNumber("SUBTRACT",5,2),3);
+    });
+
+    it("has correct output for SUBTRACT", () => {
+        assert.equal(calculateNumber("SUBTRACT",1.4,2.2),-1);
     });
 });
 
