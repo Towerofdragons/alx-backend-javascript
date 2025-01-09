aca80152c107b49a52a2c3e2e6df4bef7a887e80
Mark <marknjoropge@gmail.com>
2025-01-09 08:40:59 +0000

    Add 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -7,16 +7,16 @@ const countStudents = require('./3-read_file_async');
 const path = process.argv[2];
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.status(200).send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
   const output = countStudents(path)
   .then((output) => {
-    res.send(output);
+    res.status(200).send("This is the list of our students\n"+output);
   })
   .catch((error) => {
-    res.send(error.message);
+    res.status(500).send(error.message);
   });
 });
 
