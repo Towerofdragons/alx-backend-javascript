88606c1f9530d71c330219dc1052a7de287182c7
Mark <marknjoropge@gmail.com>
2025-01-09 06:15:52 +0000

    Fixed formatting for lint

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,42 +1,39 @@
-function countStudents(path){
-    const fs = require('fs');
-    
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf-8', (err, data) => {
-            if (err) {
-              // Reject the promise if there's an error
-              reject(new Error('Cannot load the database'));
-              return;
-            }
+const fs = require('fs');
 
-            const lines = data.split('\n').filter((line) => line.trim() !== '');
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+      // Reject the promise if there's an error
+        reject(new Error('Cannot load the database'));
+        return;
+      }
 
-            const header = lines[0].split(',');
-            const students = lines.slice(1).map((line) => line.split(','));
-    
-            console.log(`Number of students: ${students.length}`);
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-            const fields = {};
-            for (const student of students) {
-                const field = student[header.indexOf('field')];
-                const firstname = student[header.indexOf('firstname')];
-    
-                if (!fields[field]) {
-                    fields[field] = [];
-                }
-                fields[field].push(firstname);
-            }
-    
-    
-            for (const [field, names] of Object.entries(fields)) {
-                console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-              }
+      const header = lines[0].split(',');
+      const students = lines.slice(1).map((line) => line.split(','));
 
-            resolve();
+      console.log(`Number of students: ${students.length}`);
 
-        });
+      const fields = {};
+      for (const student of students) {
+        const field = student[header.indexOf('field')];
+        const firstname = student[header.indexOf('firstname')];
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+
+      for (const [field, names] of Object.entries(fields)) {
+        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+
+      resolve();
     });
-
+  });
 }
 
 module.exports = countStudents;
