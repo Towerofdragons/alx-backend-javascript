ec678dbf2dde711a120741cc70ca227b74a02940
Mark <marknjoropge@gmail.com>
2025-01-09 07:41:00 +0000

    Add http.test.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,30 +1,31 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async')
+const countStudents = require('./3-read_file_async');
+
 const path = process.argv[2];
 
-if (!path){
-    ReadableStreamDefaultController;
-}
+// if (!path) {
+//   ReadableStreamDefaultController;
+// }
 
-const app = http.createServer((request, response) =>{
-    if (request.url === '/students'){
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write("This is the list of our students\n");
-        countStudents(path)
-        .then((output) => {
-            response.write(output);
-            response.end();
-        })
-        .catch((error) => {
-            response.writeHead(500, { 'Content-Type': 'text/plain' });
-            response.end(error.message);
-        })
-    };
-    if (request.url === '/'){
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write('Hello Holberton School!');
+const app = http.createServer((request, response) => {
+  if (request.url === '/students') {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('This is the list of our students\n');
+    countStudents(path)
+      .then((output) => {
+        response.write(output);
         response.end();
-    }
+      })
+      .catch((error) => {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end(error.message);
+      });
+  }
+  if (request.url === '/') {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('Hello Holberton School!');
+    response.end();
+  }
 }).listen(1245);
 
 module.exports = app;
