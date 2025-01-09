77b1021a22a0593419083cf85c82341c9bf292de
Mark <marknjoropge@gmail.com>
2025-01-09 07:32:33 +0000

    fix http.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -8,14 +8,16 @@ function countStudents(path) {
         reject(new Error('Cannot load the database'));
         return;
       }
-
+      let output = '';
       const lines = data.split('\n').filter((line) => line.trim() !== '');
 
       const header = lines[0].split(',');
       const students = lines.slice(1).map((line) => line.split(','));
 
+      output += `Number of students: ${students.length}\n`;
       console.log(`Number of students: ${students.length}`);
 
+      
       const fields = {};
       for (const student of students) {
         const field = student[header.indexOf('field')];
@@ -26,12 +28,12 @@ function countStudents(path) {
         }
         fields[field].push(firstname);
       }
-
       for (const [field, names] of Object.entries(fields)) {
+        output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
         console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
       }
 
-      resolve();
+      resolve(output);
     });
   });
 }
--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -6,16 +6,22 @@ if (!path){
     ReadableStreamDefaultController;
 }
 
-const app = http.createServer(function(request, response){
+const app = http.createServer((request, response) =>{
     if (request.url === '/students'){
+        response.writeHead(200, {'Content-Type': 'text/html'});
         response.write("This is the list of our students\n");
         countStudents(path)
         .then((output) => {
             response.write(output);
             response.end();
         })
+        .catch((error) => {
+            response.writeHead(500, { 'Content-Type': 'text/plain' });
+            response.end(error.message);
+        })
     };
     if (request.url === '/'){
+        response.writeHead(200, {'Content-Type': 'text/html'});
         response.write('Hello Holberton School!');
         response.end();
     }
